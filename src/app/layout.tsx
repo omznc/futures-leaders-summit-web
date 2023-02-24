a843880e199c6a6c0cc1a5e7567a3ea727dede02
Omar <omznc@pop-os.localdomain>
2023-02-24 09:53:12 +0100

    Seo Stuff

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,29 @@ import Header from '@components/Header/Header';
 import { Inter } from '@next/font/google';
 import { Footer } from '@components/Footer/Footer';
 import { ReactNode } from 'react';
+import type { Metadata } from 'next';
+import { description, title } from '@helpers/seo';
 
 const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+    openGraph: {
+        title: title,
+        description: description,
+        url: 'https://fls.ba/',
+        siteName: title,
+        images: [
+            {
+                url: 'https://fls.ba/share.png',
+                width: 800,
+                height: 600,
+            },
+            ],
+        locale: 'en-US',
+        type: 'website',
+    },
+};
+
 
 export default function RootLayout({ children }: { children: ReactNode }) {
 	return (
